b9cff11bd710e0917a87021911124e1616df7273
Bohdan Pusanuk <pusanukbr@live.ru>
2018-03-08 00:24:59 +0200

    version 0.2

--- mongo.js
+++ mongo.js
@@ -1,11 +1,21 @@
 const Mongo = {
   install(Vue, options) {
-    Vue.prototype.$mongoMethod = function (methodOptions) {
-    console.log("Wo Wo Wo what is this? Plugin Mongo?")
-  },
-  Vue.prototype.$newMongoMethod = function (methodOptions) {
-    console.log("Oh yeah!!:)")
+  //   Vue.prototype.$mongoMethod = function (methodOptions) {
+  //   console.log("Wo Wo Wo what is this? Plugin Mongo?")
+  // },
+  // Vue.prototype.$newMongoMethod = function (methodOptions) {
+  //   console.log("Oh yeah!!:)")
+  // },
+  Vue.prototype.$server = function (methodOptions) {
+      this.$http.get("/api/user/get").then(function(response){
+
+        console.log(response)
+
+      },function(error){
+        //ошибка
+      })
   }
+
   }
 };
 
